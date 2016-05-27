f6af96265ba27065098b6c078f9f824240c3cd77
Chris ONeill <cjjoneill@gmail.com>
2016-05-27 14:20:45 -0600

    1

---
+++ js/animation.js
@@ -0,0 +1,34 @@
+$('#tweet-submit').hide();
+$('#char-count').hide();
+$('textarea').click(function(){
+    $('.tweet-compose').animate({height: '5em' });
+    $('#tweet-submit').show();
+    $('#char-count').show();
+
+});
+maxCharacters = 140;
+
+$('#char-count').text(maxCharacters);
+
+$('textarea').bind('keyup keydown', function() {
+    var count = $('#char-count');
+    var characters = $(this).val().length;
+
+    if (characters > maxCharacters) {
+        count.addClass('over');
+    } else {
+        count.removeClass('over');
+    }
+    count.text(maxCharacters - characters);
+    if (maxCharacters - characters < 11) {
+      $('#char-count').css({color: 'red'})
+    };
+    if (maxCharacters - characters < 0) {
+      $('#tweet-submit').hide();
+    } else {
+      $('#tweet-submit').show();
+    };
+});
+$('#tweet-submit').click(function(){
+  $('#stream').prepend('<div class="tweet"><div class="content"><img class="avatar" src="img/alagoon.jpg" /><strong class="fullname">Chris ONeill </strong><span class="username">@coneill</span><p class="tweet-text">' + $('.tweet-compose').val() + '</p><div class="tweet-actions"><ul><li><span class="icon action-reply"></span> Reply</li><li><span class="icon action-retweet"></span> Retweet</li><li><span class="icon action-favorite"></span> Favorite</li><li><span class="icon action-more"></span> More</li></ul></div></div></div>')
+});
